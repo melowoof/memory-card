2f253bba1739ab38460e72b186b6f76f8194bbe0
Ian <ian.nptk@gmail.com>
2024-12-18 22:50:24 +0100

    Added popup keyframes

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -1,18 +1,40 @@
+/* eslint-disable react/prop-types */
 import styles from "./Popup.module.css";
 import Button from "../Button/Button";
+import { useEffect, useState } from "react";
 
-// eslint-disable-next-line react/prop-types
-export default function Popup({ titleText, statusText }) {
+export default function Popup({ titleText, statusText, buttonLogic }) {
+  const [isVisible, setIsVisible] = useState(true);
+  const [isExiting, setIsExiting] = useState(false);
+
+  useEffect(() => {
+    return () => {
+      if (isExiting) {
+        setIsVisible(false);
+      }
+    };
+  }, [isExiting]);
+
+  if (!isVisible) return null;
+
+  const handleExit = () => {
+    setIsExiting(true);
+    setTimeout(() => {
+      buttonLogic();
+      setIsVisible(false);
+    }, 200);
+  };
+
   return (
-    <div className={styles.page}>
-      <div className={styles.popup}>
+    <div className={`${styles.page} ${isExiting ? styles.fade : ""}`}>
+      <div className={`${styles.popup} ${isExiting ? styles.exit : ""}`}>
         <div className={styles.background}>
           <div className={styles.foreground}>
             <div className={styles.text}>
               <div className={styles.titleText}>{titleText}</div>
               <div className={styles.statusText}>{statusText}</div>
             </div>
-            <Button text={"RESTART"} />
+            <Button text={"RESTART"} onClick={handleExit} />
           </div>
         </div>
       </div>
