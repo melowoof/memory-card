3dcc873c7e26bb6cf6efe4bd376086fe3fb81efb
Ian <ian.nptk@gmail.com>
2025-01-22 00:07:55 +0100

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Page from "./components/Page/Page.jsx";
 function App() {
   return (
     <>
-      <div className="w-screen h-[100vh]">
+      <div className="w-screen h-[100vh] sticky">
         <div className="top-0 bg-red-200">
           outside
           <div className="w-screen h-screen static bg-green-200">
