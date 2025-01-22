849f749679058e66e5691f23a8f6b6a64a9b2bbe
Ian <ian.nptk@gmail.com>
2025-01-22 01:37:35 +0100

    commented out test code

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import Page from "./components/Page/Page.jsx";
 function App() {
   return (
     <>
-      <div className="mainContainer">
+      {/* <div className="mainContainer">
         <div className="secondContainer">
           outside
           <div className="w-screen h-screen static bg-green-200">
             <div>inside * 2</div>inside
           </div>
         </div>
-      </div>
+      </div> */}
 
       <Page />
     </>
